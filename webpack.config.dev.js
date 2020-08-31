12c59707af6933cde1374a7d8d103020258cd7d8
8977shiva <b.skreddy@gmail.com>
2020-09-01 04:49:37 +0530

    env done

---
+++ webpack.config.dev.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const htmlWebpackplugin = require("html-webpack-plugin");
+
+process.env.NODE_ENV = "development";
+
+module.exports = {
+  mode: "development",
+  target: "web",
+  devtool: "cheap-module-source-map",
+  entry: "./src/index",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  plugins: [
+    new htmlWebpackplugin({
+      template: "src/index.html",
+    }),
+  ],
+  devServer: {
+    stats: "minimal",
+    overlay: true,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    https: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"],
+      },
+      {
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
